2dc8d767dfbfa3e84ea7ca2d3a259e9449868c8c
GTimothy <timothy.garwood@protonmail.com>
2017-09-26 21:42:11 +0200

    Finished display using curses and added the calculus of some statistics

--- run.py
+++ run.py
@@ -5,20 +5,27 @@ import time
 
 
 def displayMatrix(mat):
-    
-    string =" "+ " ".join([str(x) for x in xrange(len(mat))])
+    global T
+    string =" "                                  #" "+ " ".join([str(x) for x in xrange(len(mat))])
     for i, x in enumerate(mat):
-        string+="\n"+str(i)+" "+ " ".join([str(y) for y in x])
-    win.addstr(0,0,string)
+        string+= "\n"+" ".join([str(y) for y in x])  #"\n"+str(i)+" "
+    win.addstr(0,0,string+"\n\nT="+str(T))
     win.refresh()
-    time.sleep(0.2)
+    #time.sleep(0.001)
+
+def calcAlive(mat):
+    d=0
+    for i in range(num):
+       for j in range(num):
+           d+=Board_T[i][j]
+    return d
 
 
 
 def intialLifeDensity():
     aliveCells=0
     aliveCellsGoal=int(density*num*num)
-    print("Our aliveCellsGoal is : "+str(aliveCellsGoal))
+    #print("Our aliveCellsGoal is : "+str(aliveCellsGoal))
     for i in range(num):
         for j in range(num):
             Board_T[i][j] = int(random.random()<=density)
@@ -43,30 +50,64 @@ def intialLifeDensity():
 def initGame():
     global win
     global num
-    num = int(raw_input("Please chose a size for the game : "))
-    global density
-    density = float(raw_input("Please chose an intial life density for this game (between 0 and 1): "))
+    global currentLine
+    global stdscr
+    global T
+    T=0
+    currentLine= 0;
     stdscr = curses.initscr()
-
+    stdscr.addstr(currentLine, 0,"Please chose a size for the game : ")
+    currentLine+=1
+    num = int(stdscr.getstr(currentLine,0))
+    currentLine+=1
+    #num = int(raw_input("Please chose a size for the game : "))
+    global density
+    stdscr.addstr(currentLine, 0,"Please chose an intial life density for this game (between 0 and 1): ")
+    currentLine+=1
+    density = float(stdscr.getstr(currentLine,0))
+    currentLine+=1
+    #density = float(raw_input("Please chose an intial life density for this game (between 0 and 1): "))
+    convergent=0
+    avg_density=0
+    for a in range(100):
     
-    Board_a = [ num*[0] for i in range(num) ]
-    global Board_T
-    Board_T = Board_a
-    intialLifeDensity() #setting the initial board to the proper life density
-  #  print("Board_T matrix :")
-  #  displayMatrix(Board_T)
-    Board_b = [ num*[0] for i in range(num) ]
-    global Board_TPlus1
-    Board_TPlus1 = Board_b
-  #  print("\nBoard_TPlus1 matrix :")
-  #  displayMatrix(Board_TPlus1)
-    winSize = len(" "+ " ".join([str(x) for x in xrange(len(Board_T))]))
-    win = curses.newwin(10+winSize,10+winSize,5,10)
-    for i in range(100):
-        #print("\ntick   ")
-        tick()
-        displayMatrix(Board_T)
+        Board_a = [ num*[0] for i in range(num) ]
+        global Board_T
+        Board_T = Board_a
+        intialLifeDensity() #setting the initial board to the proper life density
+        #  print("Board_T matrix :")
+        #  displayMatrix(Board_T)
+        Board_b = [ num*[0] for i in range(num) ]
+        global Board_TPlus1
+        Board_TPlus1 = Board_b
+        #  print("\nBoard_TPlus1 matrix :")
+        #  displayMatrix(Board_TPlus1)
+        winWidth = len(" ".join([str(x) for x in xrange(len(Board_T))]))
+        winHeight = len(Board_T)+3
+        win = curses.newwin(winWidth,winHeight+10,currentLine+1,5)
 
+        T=0
+        for i in range(1000):
+            #print("\ntick   ")
+            tick()
+            displayMatrix(Board_T)
+        alive = calcAlive(Board_T)
+        if (alive>0):
+            convergent+=1
+        avg_density+=alive
+        
+    convergent=convergent
+    avg_density=float(avg_density)/float(convergent)/float(100)
+    
+    currentLine+=winHeight+10
+    stdscr.addstr(currentLine,0,"Prop Convergent : "+str(convergent)+"%")
+    currentLine+=1
+    stdscr.addstr(currentLine,0,"Densite Moyenne : "+str(avg_density)+"%")
+    currentLine+=1
+
+    stdscr.addstr(currentLine,0,"Press any key to exit")
+    currentLine+=1
+    stdscr.getstr(currentLine,0)
     curses.endwin()
 
 
@@ -74,6 +115,8 @@ def initGame():
 def tick():
     global Board_T
     global Board_TPlus1
+    global T
+    T+=1
     #displayMatrix(Board_T)
     #print(num)
 
@@ -108,7 +151,7 @@ def tick():
                     Board_TPlus1[i][j]=0
             else:
 #                print("Board_T["+str(i)+"]["+str(j)+"] is ALIVE")
-                if (not (Surrounding==2 or Surrounding==3)):
+                if (not (Surrounding>=2 and Surrounding<=3)):
 #                    print("\t and is surrounded by <2 or >3 alive !!! \n\t>>Becomes DEAD")
                     Board_TPlus1[i][j]=0
                 else :
